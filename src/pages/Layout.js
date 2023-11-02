178a3b20c60f3d0c591e2fc067701bbe92bebdab
Acrylate1410 <quangnm1410@gmail.com>
2023-11-02 13:54:42 +0700

    dfsg

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,3 +1,40 @@
 import { Outlet } from "react-router-dom";
-const Layout = () => {return (<Outlet />)};
+import { useEffect, useRef } from 'react';
+const Layout = () => {
+    return (
+        <>
+            <Outlet />
+            <ChatBot/>
+        </>
+)};
+
+const ChatBot = () => {
+    const MessengerRef = useRef();
+    useEffect(() => {
+        MessengerRef.current.setAttribute('page_id', '135134239690741');
+        MessengerRef.current.setAttribute('attribution', 'biz_inbox');
+    
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                xfbml: true,
+                version: 'v18.0',
+            });
+        };
+        (function (d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }, []);
+    return (
+        <>
+            <div id="fb-root"></div>
+            <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div>
+        </>
+    );
+}; 
 export default Layout;

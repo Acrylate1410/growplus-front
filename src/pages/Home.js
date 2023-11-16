cbc16a9626ffedd67aabd4e21c08ca9396307471
Acrylate1410 <quangnm1410@gmail.com>
2023-11-16 17:33:54 +0700

    gh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,6 +21,8 @@ import { Pagination } from 'swiper/modules';
 import { FaCaretDown,  FaArrowCircleUp } from "react-icons/fa";
 import Footer from './Footer'
 //[#093489]
+//bg-yellow-200
+//bg-yellow-500
 export function Home() {
   //<section className='w-full scroll-m-20' ref={sec2}><Wid /></section>
     const [articleList, setArticleList] = useState([])
@@ -112,19 +114,25 @@ export function Home() {
                 <HamburgerComponent/></div>
           </header>
           <div className="bg-[url(/public/banner.jpg)] bg-[length:158%_100%] md:bg-[length:100%_100%] h-[300px] md:h-[500px] mt-20"></div>
-          <div className='w-full md:flex'>
+          <div className='w-full md:flex justify-center'>
             <div className='md:w-1/2 mt-8 md:ml-8 mx-4 flex'><img src="chart.png" className='object-contain'/></div>
-            <div className='md:w-1/2 mt-8 md:ml-8 mx-4 flex hidden md:block'><img src="Untitled-2.png" className='object-contain'/></div>
+            <div className='md:w-[30%] mt-8 md:ml-8 mx-4 flex hidden md:block'><img src="3growplus.png" className='object-contain'/></div>
           </div>
-          <section className='w-full mt-6 mb-16 md:mb-24 md:mt-8 scroll-m-20' ref={sec1}><IngredientTab /></section>
-          <section className='w-full bg-gradient-to-r from-10% to-[#0D4CC9] from-[#093489]  pt-4 md:pt-20 md:pb-4 scroll-m-20'  ref={sec4}><NotableBenefits /></section>
+          <section className='w-full mt-6 mb-16 md:mb-24 md:mt-8 scroll-m-20\' ref={sec1}>
+
+            <IngredientTab />
+          </section>
+          <section className='w-full  bg-gradient-to-r from-10% to-[#0D4CC9] from-[#093489] md:from-white md:to-white pt-4 md:pt-20 md:pb-4 scroll-m-20 '  ref={sec4}><NotableBenefits /></section>
+          <h2 className='font-bold text-3xl md:text-4xl mx-auto text-center md:mb-8 text-white'>Ưu điểm nổi bật</h2>
+          
           <section className='w-full scroll-m-20'  ref={sec5}><Accordion /></section>
           <section className='w-full scroll-m-20'  ref={sec3}>
               <h2 className='font-bold text-3xl md:text-4xl mx-auto text-center text-[#093489]'>Quy cách đóng gói</h2>
               <div className='flex md:flex-row flex-col items-center md:justify-around py-4 px-2'>
                   <img alt="Quy cách đóng gói" src="gh.jpg" className='md:w-2/5 mt-0 md:mt-8 md:shadow-[0_60px_60px_-15px_rgba(0,0,0,0.3)] rounded-[45px]'></img>
                   <div className='w-full md:w-1/3 mt-8 md:mt-0'>
-                    <div className='flex justify-around'>
+                  
+                    <div className='flex justify-around relative'>
                         {["gói/hộp", "ml/gói"].map(i =>
                           <div className=' bg-sky-100 rounded-b-xl border-t-8 border-[#093489] text-white p-8 md:p-12 text-center w-[40%] md:mr-4 md:w-auto' key={i}>
                             <div className='rounded-full h-20 w-20 text-3xl font-bold mx-auto border-4 border-[#093489] text-[#093489] flex items-center justify-center'>30</div>
@@ -174,11 +182,22 @@ export function Home() {
               <p>Miễn phí giao hàng toàn quốc</p>
             </div>
           </section>
+
           <Footer/>
+
       </div>
     )
   };
-
+  function Block() {
+    return (
+      <>
+        <div className='relative'>
+          <div className='absolute top-0 h-[12px] bg-yellow'>Tăng chiều cao tối đa</div>
+          <div className='absolute top-0 h-[24px] flex items-center'>Các chuyên gia Nhật Bản đã xây dựng một công thức hoàn hảo không chỉ tập trung vào Canxi mà còn có các thành phần khác, giúp xương phát triển tối đa để tăng chiều cao cho trẻ và ưu việt hơn rất nhiều so với những sản phẩm tăng chiều cao thông thường chỉ tập trung vào Canxi.</div>
+        </div>
+      </>
+    )
+  }
   function Form() {
     const [toggle, setToggle] = useState("hidden")
     const name = useRef(null)
@@ -213,14 +232,15 @@ export function Home() {
             <div className='relative z-10 backdrop-blur-[0.5px] h-full  flex flex-col justify-center'>
               <h2 className='text-center font-bold text-3xl md:text-4xl  mb-4 z-10 text-white'>Thông tin giao hàng</h2>
               <form onSubmit={handleSubmit} className='flex flex-col items-center'>
-                <input ref={name} required className='outline-0 text-[#093489] md:w-1/2 w-4/5 pl-5 py-2 my-2 rounded-lg' placeholder='Họ và tên khách hàng'></input>
+                <input ref={name} required className='outline-0 text-[#093489] md:w-1/2 w-4/5 px-5 py-2 my-2 rounded-lg' placeholder='Họ và tên khách hàng'></input>
                 <div className='relative md:w-1/2 w-4/5 '>
                   <div className='outline-0 text-zinc-500 pl-5 py-2  my-2 rounded-lg w-full bg-white h-10 cursor-pointer' onMouseDown={() => {toggle === "hidden" ? setToggle("") : setToggle("hidden")}}>Số điện thoại & Địa chỉ nhận hàng</div>
                   <div className='absolute top-4 right-4 text-2xl'  onMouseDown={() => {toggle === "hidden" ? setToggle("") : setToggle("hidden")}} ><FaCaretDown /></div>
                 </div>
                 <div className={'md:w-1/2 w-4/5 '  + toggle}>
-                  <input ref={phone} required className='outline-0 text-[#093489] w-full pl-5 py-2 my-2 rounded-lg' placeholder='Số điện thoại'></input>
-                  <input ref={address} required className='outline-0 text-[#093489] w-full pl-5 py-2 my-2 rounded-lg' placeholder='Đường, phường/xã, quận/huyện, thành phố/tỉnh'></input>
+                  <input ref={phone} required className='outline-0 text-[#093489] w-full px-5 py-2 my-2 rounded-lg' placeholder='Số điện thoại'></input>
+                  <input ref={address} required className='outline-0 text-[#093489] w-full px-5 py-2 my-2 rounded-lg' placeholder='Đường, phường/xã, quận/huyện, thành phố/tỉnh'></input>
+                  <input ref={name} required className='outline-0 text-[#093489] w-full px-5 py-2 my-2 rounded-lg' placeholder='Ghi chú'></input>
                 </div>
                 <div className='mx-auto md:w-1/2 w-4/5 flex justify-end items-center mt-4'>
                   <p className='text-white'>Số lượng: </p>
@@ -263,21 +283,23 @@ export function Home() {
                        ]
     return (
       <>
-
-        <div id="ingredients" className='md:w-3/5 md:ml-32 mx-2  '>
+        <div id="ingredients" className='md:w-3/5 mx-auto md:ml-44 relative'>
+        < img className='hidden md:inline absolute right-[-300px] bottom-0 w-96' src="Growplusbboxthanh.png"></img>
           <h2 className='text-center font-bold text-3xl md:text-4xl  mb-5 z-10 text-[#093489] relative'>Thành phần dinh dưỡng</h2>
-          <div className='absolute right-4 overflow-hidden w-96 z-0 mt-20 hidden md:block' ><img src="3growplus.png"></img></div>
-          <div className='flex justify-between font-bold px-4 py-5 text-white rounded-t-xl bg-gradient-to-r from-10% to-[#0D4CC9] from-[#093489] items-center '>
+          <div className='flex justify-between font-bold px-4 py-5 text-white rounded-t-xl bg-gradient-to-r from-10% to-[#0D4CC9] from-[#093489] items-center  md:w-[94%]'>
               <p>Thành phần  (trong 1 gói 30ml)</p>
               <p className='text-end'>Hàm lượng (mg/gói)</p>
           </div>
-          <div className='h-[500px] overflow-y-scroll'>
+          <div className='h-[500px] overflow-y-scroll  shadow-[0_60px_60px_-15px_rgba(0,0,0,0.3)] md:w-[94%]'>
+
               {ingredients.map(i =>
                 <div key={i.id} className={i.id % 2 !== 0 ? 'flex justify-between py-4 px-4' :  'flex justify-between py-4 px-4 bg-sky-100'}>
                     <p className='font-semibold'>{i.id + ". " + i.ingredient}</p>
                     <p className='font-semibold'>{i.amount}</p>
                 </div>
               )}
+          </div>
+          <div className='py-2 bg-white rounded-b-lg  md:w-[94%]'>
           </div>
         </div>
       </>
@@ -286,26 +308,42 @@ export function Home() {
 
   
   function NotableBenefits() {
-    const texts = [ {id: "Bảng thành phần vàng", text: "Sản phẩm hàng đầu của Nhật Bản về SỰ PHÁT TRIỂN TOÀN DIỆN đặc biệt là SỰ PHÁT TRIỂN CHIỀU CAO của trẻ với tổng hợp 23 thành phần chọn lọc."},
+    //{id: "Bảng thành phần vàng", text: "Sản phẩm hàng đầu của Nhật Bản về SỰ PHÁT TRIỂN TOÀN DIỆN đặc biệt là SỰ PHÁT TRIỂN CHIỀU CAO của trẻ với tổng hợp 23 thành phần chọn lọc."},
+    const texts1 = [ 
                     {id: "Tăng chiều cao tối đa", text: "Các chuyên gia Nhật Bản đã xây dựng một công thức hoàn hảo không chỉ tập trung vào Canxi mà còn có các thành phần khác, giúp xương phát triển tối đa để tăng chiều cao cho trẻ và ưu việt hơn rất nhiều so với những sản phẩm tăng chiều cao thông thường chỉ tập trung vào Canxi."},
                     {id: "Hệ tiêu hóa khỏe mạnh", text: "Đây là một sản phẩm với công thức không chỉ giúp hấp thụ nhóm các chất dinh dưỡng để tăng chiều cao từ sản phẩm mà còn hấp thụ Canxi tự nhiên bằng cách đề cao nhóm 3 lợi khuẩn Axit lactic, Bifidobacteria và Oligosaccharide, giúp trẻ có 1 hệ tiêu hóa khỏe mạnh, giúp trẻ hấp thu và chuyển hóa các chất dinh dưỡng ở mức tối ưu nhất."},
                     {id: "Tăng cường hệ miễn dịch", text: "Thành phần có vitamin tổng hợp, keo ong… và các dưỡng chất cần thiết khác giúp trẻ có có hệ hô hấp và cơ thể khỏe mạnh, phòng ngừa sự tấn công của các loại vi khuẩn và vi rút, tăng cường sức đề kháng và khả năng miễn dịch."},
-                    {id: "Hỗ trợ phát triển não bộ", text: "Sản phẩm không chỉ giúp trẻ phát triển về thể chất, chiều cao mà còn giúp trẻ tăng cường trí nhớ, phát triển trí thông minh nhờ có thành phần DHA, EPA và Phosphatidylserine giúp cải thiện chức năng não bộ, phát triển kích thích tế bào não."},
+]
+    const texts2 = [
+                     {id: "Hỗ trợ phát triển não bộ", text: "Sản phẩm không chỉ giúp trẻ phát triển về thể chất, chiều cao mà còn giúp trẻ tăng cường trí nhớ, phát triển trí thông minh nhờ có thành phần DHA, EPA và Phosphatidylserine giúp cải thiện chức năng não bộ, phát triển kích thích tế bào não."},
                     {id: "Mùi vị thơm ngon, tiện lợi sử dụng", text: "Mùi vị thơm ngon, độ ngọt vừa phải và không có đường có thể gây sâu răng cho trẻ; Bào chế dạng nước có khả năng hấp thu tốt nhất cho cơ thể so với các dạng bào chế khác; 1 gói 30ml tiện lợi để sử dụng và bảo quản."},
-                    {id: "Đạt tiêu chuẩn GMP Nhật Bản", text: "Sản xuất tại nhà máy đạt chuẩn GMP có tiêu chuẩn chất lượng khắt khe về nguyên liệu, máy móc, công nghệ, quy trình sản xuất và chất lượng thành phẩm với đội ngũ chuyên gia hàng đầu nghiên cứu về nguồn dinh dưỡng & phát triển toàn diện của trẻ"}]
+                    {id: "Đạt tiêu chuẩn GMP Nhật Bản", text: "Sản xuất tại nhà máy đạt chuẩn GMP có tiêu chuẩn chất lượng khắt khe về nguyên liệu, máy móc, công nghệ, quy trình sản xuất và chất lượng thành phẩm với đội ngũ chuyên gia hàng đầu nghiên cứu về nguồn dinh dưỡng & phát triển toàn diện của trẻ"}
+    ]
     return (
       <>
-        <h2 className='font-bold text-3xl md:text-4xl mx-auto text-center md:mb-8 text-white'>Ưu điểm nổi bật</h2>
+        <h2 className='font-bold text-3xl md:text-4xl mx-auto text-center md:mb-8 text-white md:text-[#093489]'>Ưu điểm nổi bật</h2>
         <div className='hidden md:flex flex-wrap justify-between md:my-16 md:mx-12'>
-          {texts.map(i =>
-            <div className='w-[90%] md:w-[31.5%] my-8 md:my-4 mx-auto md:mx-0 bg-white pt-8 pb-12 rounded-3xl'>
-                  <div className='text-xl mx-6 md:text-2xl font-bold text-[#093489] mb-8'>{i.id}</div>
-                  <div className='flex-1 mx-6'>{i.text}</div>
-            </div>
-          )}
+          <div className='w-[30%]'>
+              {texts1.map(i =>
+                <div className='w-full my-8 md:my-4 mx-auto md:mx-0 bg-white pt-20 pb-4 rounded-3xl relative md:shadow-[0_60px_60px_-15px_rgba(0,0,0,0.3)] rounded-[45px]'>
+                      <div className='absolute h-16 w-full bg-black top-0 rounded-t-3xl text-white bg-gradient-to-r from-yellow-500 via-yellow-300 to-yellow-500 text-black flex items-center justify-center font-bold text-lg'>{i.id}</div>
+                      <div className='flex-1 mx-6 text-justify'>{i.text}</div>
+                </div>
+              )}
+          </div>
+          <img src="Thanh.png" className='w-[30%] object-contain'></img>
+          <div className='w-[30%]'>
+              {texts2.map(i =>
+                <div className='w-full my-8 md:my-4 mx-auto md:mx-0 bg-white pt-20 pb-4 rounded-3xl relative md:shadow-[0_60px_60px_-15px_rgba(0,0,0,0.3)] rounded-[45px]'>
+                      <div className='absolute h-16 w-full bg-black top-0 rounded-t-3xl text-white bg-gradient-to-r from-yellow-500 via-yellow-300 to-yellow-500 text-black flex items-center justify-center font-bold text-lg'>{i.id}</div>
+                      <div className='flex-1 mx-6 text-justify'>{i.text}</div>
+                </div>
+              )}
+          </div>
+
         </div>
         <Swiper spaceBetween={30} pagination={true} modules={[Pagination]} className='mb-8 !mx-8 md:!hidden'>
-                {texts.map(i =>
+                {texts1.map(i =>
                   <SwiperSlide><div className='w-[100%] md:w-[31.5%] mt-4 mb-8 md:my-4 mx-auto md:mx-0 bg-white pt-2 pb-12 rounded-3xl h-96'>
                     <div className='text-xl mx-6 md:text-2xl font-bold text-[#093489] md:mb-8 text-center'>{i.id}</div>
                       <div className='flex-1 mx-6 text-justify'>{i.text}</div>
@@ -360,6 +398,7 @@ export function Home() {
     )
   }
 */
+  
   function Accordion() {
     const texts = ["Liều dùng: 1 gói 1 ngày", "Nên uống buổi sáng sau hoặc trước khi ăn 30 phút đến 1 tiếng", "Nên sử dụng theo liệu trình tối thiểu 3 đến 5 tháng để đạt hiệu quả tối đa", "Sản phẩm có thể sử dụng duy trì thường xuyên mà không gây tác dụng phụ"]
     return (

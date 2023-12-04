7436de5d88ae387dfaddc06e44d546788394ef98
Acrylate1410 <quangnm1410@gmail.com>
2023-12-04 16:27:20 +0700

    hjk

--- src/pages/Hiring.js
+++ src/pages/Hiring.js
@@ -12,13 +12,13 @@ export function Hiring() {
                 </div>
               </Link>
       </header>
-        <div className="mt-24 w-[90%] md:w-3/5 text-justify mx-auto">
+        <div className="mt-24 w-[90%] md:w-[65%] text-justify mx-auto">
         <h1 className=" font-bold text-3xl text-center mx-2 mt-4 mb-8 text-[#093489]">TUYỂN ĐẠI LÝ GROW PLUS+ CHÍNH HÃNG</h1>
         <p>Nếu bạn đang:</p>
         <p>- Muốn kinh doanh sản phẩm "mẹ và bé" nhưng chưa biết phải lựa chọn mặt hàng nào uy tín, chất lượng cao.</p>
         <p>- Là đại lý phân phối các loại TPCN trên thị trường và muốn mở rộng thêm các sản phẩm đến từ Nhật Bản có chất lượng khác</p>
         <p>- Muốn trở thành đại lý/nhà phân phối chính hãng cho 1 thương hiệu, sản phẩm chất lượng, có thể đem lại thu nhập tốt hàng tháng.</p>
-        <p>Trở thành nhà phân phối của GROW PLUS + sẽ là lựa chọn tuyệt vời dành cho bạn với những quyền lợi:</p>
+        <p>Trở thành nhà phân phối của GROW PLUS+ sẽ là lựa chọn tuyệt vời dành cho bạn với những quyền lợi:</p>
         <p>- Chính sách chiết khấu hấp dẫn</p>
         <p>- Cơ chế hợp tác linh hoạt</p>
         <p>- Được đào tạo về sản phẩm và kỹ năng tư vấn bán hàng</p>
